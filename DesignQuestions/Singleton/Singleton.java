68b6ec1a09b251c24bb38eaaca265fe5522ae77f
Harish Chellaboyina <harish.chellaboyina@kony.com>
2017-08-15 21:30:13 +0530

    Adding singleton pattern

---
+++ DesignQuestions/Singleton/Singleton.java
@@ -0,0 +1,16 @@
+package DesignQuestions.Singleton;
+
+public class Singleton {
+	
+	private static Singleton obj;
+	
+	private Singleton(){
+		
+	}
+	
+	public static Singleton getInstance() {
+		if (obj == null)
+			obj = new Singleton();
+		return obj;
+	}
+ }
